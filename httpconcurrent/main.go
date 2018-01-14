cf4b731c12921c4d78ee53d21217df2d9f5a5d95
kholidfu <sopier@gmail.com>
2018-01-14 19:58:15 +0700

    learning concurrent download in Go

---
+++ httpconcurrent/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"strings"
+)
+
+func main() {
+	readFile()
+	images := readFile()
+	for _, image := range images {
+		image = strings.TrimRight(image, "\r\n")
+		fmt.Printf("downloading %s\n", image)
+		fname := getFileName(image)
+		err := download("/tmp/"+fname, image)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func getFileName(url string) string {
+	return path.Base(url)
+}
+
+func download(fpath, url string) (err error) {
+	// Create the file
+	out, err := os.Create(fpath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Writer the response body to file
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// read from file
+// return list of url to the image
+func readFile() []string {
+
+	var images []string
+	var line string
+
+	pathToFile := strings.Join([]string{"/home", "kholidfu", "Desktop", "images.txt"}, "/")
+	// check if file exist
+	if _, err := os.Stat(pathToFile); err == nil {
+		// read the file
+		f, err := os.Open(pathToFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		reader := bufio.NewReader(f)
+		for {
+			line, err = reader.ReadString('\n')
+			// check if line not blank
+			if len(line) > 0 {
+				images = append(images, line)
+			}
+			if err != nil {
+				break
+			}
+		}
+	}
+	return images
+}
