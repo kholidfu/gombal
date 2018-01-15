5efa326e05d1da2e99e873204c2d54f307371e8e
kholidfu <sopier@gmail.com>
2018-01-16 05:44:52 +0700

    fix the make(chan

--- goroutine/main.go
+++ goroutine/main.go
@@ -19,13 +19,14 @@ func main() {
 		"https://www.google.com",
 		"https://www.detik.com",
 		"https://www.yahoo.com",
+		"http://88gram.com", // return 500, should be last shown
 		"https://www.bing.com",
 		"https://www.duckduckgo.com",
 		"https://www.golang.org",
 		"https://www.github.com",
 		"https://www.python.org",
 	}
-	ch := make(chan int)
+	ch := make(chan int, len(urls))
 	for _, url := range urls {
 		go checkStatus(url, ch)
 	}
