741455d71f7454c25f0efcbc316f871131a458f8
kholidfu <sopier@gmail.com>
2018-01-15 20:14:04 +0700

    simple example on goroutine in Golang

---
+++ goroutine/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func checkStatus(url string, ch chan<- int) {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ch <- resp.StatusCode
+}
+
+func main() {
+	urls := []string{"https://www.google.com", "https://mslib.co", "https://www.decorpad.com"}
+	ch := make(chan int)
+	for _, url := range urls {
+		go checkStatus(url, ch)
+	}
+	for range urls {
+		fmt.Println(<-ch)
+	}
+}
